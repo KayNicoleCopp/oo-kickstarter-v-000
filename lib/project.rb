76c31edc1c74f355d64062ef57a4b8a4b83c0319
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-11-15 17:13:24 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,15 @@
 class Project
-attr_accessor :title
+attr_accessor :title, :backers
 
 def intialize(title)
   @title = title
+  @backers = []
 end
 
+def add_backer(backer)
+  @backers << backer
+  backer.back_project(self) unless backer.backed_projects.include?(self)
+end
 
 
 end
