67eccf55c99b147f76b250bc30b09665b1390c64
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-11-15 17:32:41 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,8 +7,8 @@ def initialize(title)
 end
 
 def add_backer(backer)
-  self.backers << backer
-  backer.back_project << self
+  @backers << backer
+  backer.back_project(self) unless backer.backed_projects.include?(self)
 end
 
 
